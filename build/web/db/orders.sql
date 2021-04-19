dd09b83ae0135e295481c0b3e7da6cbdaf370602
DeanBedingfield2 <81820518+DeanBedingfield2@users.noreply.github.com>
2021-04-19 16:29:33 +1000

    database fix 2

---
+++ build/web/db/orders.sql
@@ -0,0 +1,5 @@
+CREATE TABLE ORDERS (
+    order_id int,
+    total_price decimal(15),
+    CONSTRAINT orderpk PRIMARY KEY (order_id)
+);
---
+++ build/web/db/products.sql
@@ -0,0 +1,7 @@
+    CREATE TABLE PRODUCTS (
+    product_id int,
+    product_name varchar(254),
+    price decimal(10),
+    discount int,
+    CONSTRAINT productpk PRIMARY KEY (product_id)
+);
