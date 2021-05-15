e7182c162c9705229d20aef5faa2742e852a864f
lilileah <13197761@student.uts.edu.au>
2021-05-16 00:21:37 +1000

    Revert "Revert "Merge branch 'main' of https://github.com/lilileah/IoTBay1 into main""
    This reverts commit 914702b012781588b428fe68eca836090bb7abb1.

--- web/db/payment_details.sql
+++ web/db/payment_details.sql
@@ -1,7 +1,10 @@
-    CREATE TABLE PAYMENT_DETAILS (
-    payment_details_id int,
-    card_name varchar(254),
-    card_no int,
+       CREATE TABLE PAYMENT_DETAILS (
+    payment_details_id int NOT NULL GENERATED ALWAYS AS IDENTITY,
+    card_type varchar(30),
+    card_number BIGINT NOT NULL,
+    owner_name varchar(50),
+    exp_date varchar(20),
+    CCV int NOT NULL,
     user_id int,
     CONSTRAINT payment_details_id_pk PRIMARY KEY (payment_details_id),
     CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES users(user_id)
