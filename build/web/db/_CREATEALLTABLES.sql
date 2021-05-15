914702b012781588b428fe68eca836090bb7abb1
lilileah <13197761@student.uts.edu.au>
2021-05-16 00:18:50 +1000

    Revert "Merge branch 'main' of https://github.com/lilileah/IoTBay1 into main"
    This reverts commit 60cd8d30df7052bced6dfe5338d6173327e6fe84, reversing
    changes made to 712bd8bd901d28ce018b29d945646838b8c015a5.

--- build/web/db/_CREATEALLTABLES.sql
+++ build/web/db/_CREATEALLTABLES.sql
@@ -83,13 +83,10 @@ CREATE TABLE ORDERS (
     CONSTRAINT order_id_pk PRIMARY KEY (order_id)
 );
 
-       CREATE TABLE PAYMENT_DETAILS (
+    CREATE TABLE PAYMENT_DETAILS (
     payment_details_id int NOT NULL GENERATED ALWAYS AS IDENTITY,
-    card_type varchar(30),
-    card_number BIGINT NOT NULL,
-    owner_name varchar(50),
-    exp_date varchar(20),
-    CCV int NOT NULL,
+    card_name varchar(254),
+    card_no int,
     user_id int,
     CONSTRAINT payment_details_id_pk PRIMARY KEY (payment_details_id),
     CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES users(user_id)
--- web/db/payment_details.sql
+++ web/db/payment_details.sql
@@ -1,10 +1,7 @@
-       CREATE TABLE PAYMENT_DETAILS (
-    payment_details_id int NOT NULL GENERATED ALWAYS AS IDENTITY,
-    card_type varchar(30),
-    card_number BIGINT NOT NULL,
-    owner_name varchar(50),
-    exp_date varchar(20),
-    CCV int NOT NULL,
+    CREATE TABLE PAYMENT_DETAILS (
+    payment_details_id int,
+    card_name varchar(254),
+    card_no int,
     user_id int,
     CONSTRAINT payment_details_id_pk PRIMARY KEY (payment_details_id),
     CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES users(user_id)
